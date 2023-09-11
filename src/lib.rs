6963ac69a3ac03cc0963c3b9e72d08581ad11e72
Piotr Sarna <sarna@turso.tech>
2023-09-11 13:28:05 +0200

    one more

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,13 @@ fn handle_turso_bot(req: Request) -> Result<Response> {
     let telegram_bot = config::get("telegram_bot_token")?;
     let api_endpoint = format!("https://api.telegram.org/bot{telegram_bot}/sendMessage");
 
+    // TODO: handle /start and other commands
+
     let params: serde_json::Value =
         serde_json::from_slice(req.body().as_deref().unwrap_or_default())?;
     let message = params.get("message").context("message not found")?;
 
-    let chat_id = message.get("chat_id").context("chat_id field not found")?;
+    let chat_id = message.get("chat").context("chat field not found")?;
     let text = message.get("text").context("text field not found")?;
     let response = format!("Hey, Turso bot here, thanks for sending '{text}', much appreciated!");
     let response = urlencoding::encode(&response);

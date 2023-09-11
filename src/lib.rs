d81c534b2fa53b409f4178bfd519488111545205
Piotr Sarna <sarna@turso.tech>
2023-09-11 13:46:23 +0200

    edited message

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,10 @@ fn handle_turso_bot(req: Request) -> Result<Response> {
 
     let params: serde_json::Value =
         serde_json::from_slice(req.body().as_deref().unwrap_or_default())?;
-    let message = params.get("message").context("message not found")?;
+    let message = params
+        .get("message")
+        .unwrap_or_else(|| params.get("edited_message"))
+        .context("message not found")?;
 
     let chat = message.get("chat").context("chat field not found")?;
     let chat_id = chat.get("id").context("chat id field not found")?;
